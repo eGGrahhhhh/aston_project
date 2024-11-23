eafdc4ce619d015dbfb74f49d2a0b70c311a1dab
eGGrahhhhh <epilik1@gmail.com>
2024-11-23 15:40:38 +0300

    Выполненные задания 4-8

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,16 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        printThreeWords();
-        checkSumSign();
-        printColor();
+        printThreeWords(); //Задание 1
+        checkSumSign(); //Задание 2
+        printColor(); //Задание 3
+        compareNumbers(); //Задание 4
+        sumCheck(1, 11); //Задание 5
+        positiveOrNegativeNumber(1); //Задание 6
+        positiveOrNegativeNumber2(1); //Задание 7
+        printText("some text", 3); //Задание 8
 
     }
 
@@ -35,6 +38,46 @@ public class Main {
             System.out.println("Желтый");
         } else {
             System.out.println("Зеленый");
+        }
+    }
+
+    public static void compareNumbers() {
+        int a = 1, b = 6;
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+
+    public static boolean sumCheck(int a, int b) {
+        int sum = a + b;
+        if (sum > 10 && sum <= 20) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void positiveOrNegativeNumber(int a) {
+        if (a >= 0) {
+            System.out.println("Положительное");
+        } else {
+            System.out.println("Отрицательное");
+        }
+    }
+
+    public static boolean positiveOrNegativeNumber2(int a) {
+        if (a >= 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public static void printText(String str,int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println(str);
         }
     }
 }
