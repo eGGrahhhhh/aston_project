e1622ee7e51a8b6d2d0da464236c15c8ec5b1a6b
eGGrahhhhh <epilik1@gmail.com>
2024-11-27 13:03:46 +0300

    Выполненные задания 1

---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,35 @@
+package org.example;
+
+public class Person {
+
+    private String fullName;
+    private String position;
+    private String email;
+    private String number;
+    private int salary;
+    private int age;
+
+    public Person(String fullName, String position, String email, String number, int salary, int age) {
+        this.fullName = fullName;
+        this.position = position;
+        this.email = email;
+        this.number = number;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public void printInfo() {
+        System.out.println("Full Name: " + fullName);
+        System.out.println("Position: " + position);
+        System.out.println("Email: " + email);
+        System.out.println("Phone: " + number);
+        System.out.println("Salary: " + salary);
+        System.out.println("Age: " + age);
+    }
+
+    public static void main(String[] args) {
+        Person person
+                = new Person("Ivanov Ivan", "Engineer", "ivivan@gmail.com", "802312312", 30000, 30);
+        person.printInfo();
+    }
+}
