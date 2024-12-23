9255c09ab899514c3ddd7f888a807480aaff6b04
eGGrahhhhh <epilik1@gmail.com>
2024-12-23 16:10:50 +0300

    Практическое задание Л15. Реализованны 4 теста.

---
+++ src/main/java/org/example/lesson_15/Main.java
@@ -0,0 +1,14 @@
+package org.example.lesson_15;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "D:/AQA_ASTON/aston_project/src/main/resources");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://google.com");
+
+    }
+}
